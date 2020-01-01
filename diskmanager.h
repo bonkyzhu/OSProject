ccdc94b290084d6eae6fea72bd9e032a7cb86fa9
zhuziquan <mymint@my.com>
2020-01-01 23:51:31 +0800

    upload

--- diskmanager.h
+++ diskmanager.h
@@ -1,14 +1,22 @@
-//
-// Created by Bonky on 2020/1/1.
-//
-
-#ifndef 课设_DISKMANAGER_H
-#define 课设_DISKMANAGER_H
-
+/*
+ * z作用
+ */
+#include <iostream>
+#include <vector>
+#include "inode.h"
+#include "disk.h"
+using namespace std;
 
 class diskmanager {
-
+  public:
+    int WriteData(string data); // 写入数据区，满了的话就返回0，成功写入返回1
+    string Read(int tread_id); // 不确定需要 message 吗
+    int DeleteData(int tread_id); // 删除数据，
+    int swap(int thread_id, int datasize, string message, string name);
+    int DelSwap(int thread_id);
+  private:
+    disk thedisk; // 代表disk
+    vector<inode> inodes; // tread_id 就是序号
 };
 
 
-#endif //课设_DISKMANAGER_H
